9fe9ae364911b4f7c706e74234f2dbbc6c8b0647
ChandrPr <praphulla91@gmail.com>
2024-11-19 07:44:23 +0000

    final

---
+++ app/models/board.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Board < ApplicationRecord
+
+  has_many(:active_posts, -> { active }, class_name: "Post", foreign_key: "board_id")
+  has_many(:expired_posts, -> { expired }, class_name: "Post", foreign_key: "board_id")
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  expires_on :date
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :integer
+#
+class Post < ApplicationRecord
+
+  validates :body, :presence => true
+
+  scope :active, -> { where("expires_on >= ? ", Date.today) }
+  scope :expired, -> { where("expires_on < ?", Date.today) }
+
+  belongs_to(:board, class_name: "Board", foreign_key: "board_id")
+
+end
